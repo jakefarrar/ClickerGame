6721bd008f1cbef4eccc817208afe98600e33bf6
email <cws73@nau.edu>
2018-11-25 13:12:34 -0700

    changed the image view to a image button added animation to button press score now updates on button press

--- app/src/main/java/com/example/jakefarrar/clickergame/MainActivity.java
+++ app/src/main/java/com/example/jakefarrar/clickergame/MainActivity.java
@@ -2,6 +2,13 @@ package com.example.jakefarrar.clickergame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Animation animScale = AnimationUtils.loadAnimation(this, R.anim.scale);
+        final TextView scoreText = findViewById(R.id.textView_score);
+
+        ImageButton targetButton = findViewById(R.id.imageButton4);
+
+        targetButton.setOnClickListener(
+                new Button.OnClickListener()
+                {
+                    public void onClick(View v)
+                    {
+                        v.startAnimation(animScale);
+                        String currentScoreString = scoreText.getText().toString();
+                        int currentScoreInt = Integer.parseInt(currentScoreString);
+                        currentScoreInt += 1;
+                        scoreText.setText(Integer.toString(currentScoreInt));
+                    }
+                }
+        );
     }
 }
