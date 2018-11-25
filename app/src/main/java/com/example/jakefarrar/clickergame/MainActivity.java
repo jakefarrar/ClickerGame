f7bfe004ffb678809d649709fb4c45d2269ea915
email <cws73@nau.edu>
2018-11-25 13:52:59 -0700

    added a timer

--- app/src/main/java/com/example/jakefarrar/clickergame/MainActivity.java
+++ app/src/main/java/com/example/jakefarrar/clickergame/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.jakefarrar.clickergame;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,10 +8,27 @@ import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView timerTextView;
+    long startTime = 0;
+
+    Handler timerHandler = new Handler();
+    Runnable timerRunnable = new Runnable() {
+        @Override
+        public void run() {
+            long millis = System.currentTimeMillis() - startTime;
+            int seconds = (int) (millis / 1000);
+            int minutes = seconds / 60;
+            seconds = seconds % 60;
+
+            timerTextView.setText(String.format("%d:%02d", minutes, seconds));
+
+            timerHandler.postDelayed(this, 500);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +37,11 @@ public class MainActivity extends AppCompatActivity {
 
         final Animation animScale = AnimationUtils.loadAnimation(this, R.anim.scale);
         final TextView scoreText = findViewById(R.id.textView_score);
+
+        timerTextView = findViewById(R.id.timerTextView);
+        startTime = System.currentTimeMillis();
+        timerHandler.postDelayed(timerRunnable, 0);
+
 
         ImageButton targetButton = findViewById(R.id.imageButton4);
 
